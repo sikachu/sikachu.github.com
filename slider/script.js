cd586309ed2950567c163c7a899c8d3c7b0ef82b
Prem Sichanugrist <s@sikachu.com>
2010-03-24 22:23:37 -0400

    Add slider demo :)

---
+++ slider/script.js
@@ -0,0 +1,21 @@
+var moving = false
+
+$(document).ready(function(){
+  $('#slider .photo').mouseover(function(){
+    if($(this).hasClass('active')){ return; }
+    
+    $(this).animate({opacity: 0.7}, {queue: false, complete: function(){
+      $(this).animate({opacity: 1}, {queue: false})
+    }})
+  }).click(function(){
+    if($(this).hasClass('active') || moving){ return; }
+    moving = true;
+    
+    $('#slider .photo.active').animate({width: '46px'}, 1500, function(){
+      $(this).removeClass('active');
+    })
+    $(this).animate({width: '752px'}, 1500, function(){
+      moving = false;
+    }).addClass('active');
+  });
+})
